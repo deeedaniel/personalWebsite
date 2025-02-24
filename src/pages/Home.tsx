02ffac53ebae14022d1b3ca855e991fa4f9c2422
Daniel <nguyendaniel1312@gmail.com>
2025-02-23 18:58:41 -0800

    revamp with react ts typescript

---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
+export default Home;
---
+++ src/pages/Resume.tsx
@@ -0,0 +1,9 @@
+function Resume() {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <p>This is the resume page.</p>
+    </div>
+  );
+}
+export default Resume;
