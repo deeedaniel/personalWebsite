458feda0da5527c66ef65a6c641fbafeca13ad00
Daniel <nguyendaniel1312@gmail.com>
2025-02-24 11:59:47 -0800

    Configure for github page deployment again

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/personalWebsite/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
